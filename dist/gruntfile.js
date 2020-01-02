9dbb76f313f01d4e378f13afbc48ad40d7ef346c
Mark Handy <mjhandy@outlook.com>
2020-01-02 10:52:40 -0500

    no message

--- dist/gruntfile.js
+++ dist/gruntfile.js
@@ -7,12 +7,25 @@ module.exports = function (grunt) {
   grunt.initConfig({
 
     pkg: this.file.readJSON('package.json'),
-    info:{
+    info: {
       bootstrap: 'bootstrap',
       configDir: 'frontEndConfigFiles',
       components: '../components',
+      scripts: '../scripts',
       scss: 'scss',
       src: '../src'
+    },
+    // CSS Comb
+    csscomb: {
+      options: {
+        config: '<%= info.configDir %>/.csscomb.json'
+      },
+      dev: {
+        expand: true,
+        cwd: '<%= info.components %>',
+        src: ['**/*.scss'],
+        dest: '<%= info.components %>',
+      }
     },
     // build the scss file
     sass: {
@@ -25,7 +38,7 @@ module.exports = function (grunt) {
           ]
         },
         files: {
-          '<%= info.src %>/css/main.css' : '<%= info.scss %>/main.scss'
+          '<%= info.src %>/css/main.css': '<%= info.scss %>/main.scss'
         }
       }
     },
@@ -39,25 +52,88 @@ module.exports = function (grunt) {
         // front end
         '<%= info.components %>/**/*.js',
       ]
-    },  
+    },
+    // format js files
+    jsbeautifier: {
+      options: {
+        config: '<%= info.configDir %>/.jsbeautifyrc'
+      },
+      dev: {
+        src: [
+          '<%= info.components %>/**/*.js',
+          '<%= info.scripts %>/global.js'
+        ]
+      }
+    },
+    // combine and minify js files
+    uglify: {
+      // v3
+      prod: {
+        options: {
+          mangle: true,
+          beautify: false,
+          compress: {
+            drop_console: true
+          },
+          comments: 'false',
+          quoteStyle: 1,
+          banner: '// <%= pkg.name %> ' + ': Prod Complied Date: <%= grunt.template.today("yyyy-mm-dd HH:mm:ss") %> ',
+          footer: '\n // EOF'
+        },
+        src: [
+          '<%= info.bootstrap%>/dist/js/bootstrap.bundle.min.js',
+          '<%= info.components %>/**/*.js',
+          '<%= info.scripts %>/global.js'
+        ],
+        dest: '<%= info.src %>/js/main.js'
+      },
+      dev: {
+        options: {
+          mangle: false,
+          beautify: true,
+          comments: true,
+          quoteStyle: 1,
+          banner: '// <%= pkg.name %> ' + ': Dev Complied Date: <%= grunt.template.today("yyyy-mm-dd HH:mm:ss") %> '
+        },
+        src: [
+          '<%= info.bootstrap%>/dist/js/bootstrap.bundle.min.js',
+          '<%= info.components %>/**/*.js',
+          '<%= info.scripts %>/global.js'
+        ],
+        dest: '<%= info.src %>/js/main.js'
+      }
+    },
     // watch files
-    watch:{
-      sass:{
-        files:[ '../components/**.scss','scss/**.scss' ],
+    watch: {
+      cssComb: {
+        files: ['../components/**.scss', 'scss/**.scss'],
+        tasks: ['csscomb']
+      },
+      sass: {
+        files: ['../components/**.scss', 'scss/**.scss'],
         tasks: ['sass:dev']
       },
-      jshint:{
-        files:['<%= info.components %>/**/*.js'],
+      jshint: {
+        files: ['<%= info.components %>/**/*.js, <%= info.scripts %>/global.js'],
         taskas: ['jshint']
+      },
+      jsbeautifier: {
+        files: ['<%= info.components %>/**/*.js, <%= info.scripts %>/global.js'],
+        taskas: ['jsbeautifier']
+      },
+      uglify: {
+        files: ['<%= info.components %>/**/*.js, <%= info.scripts %>/global.js'],
+        taskas: ['uglify:dev']
       }
     }
 
   });
 
 
-  grunt.registerTask('dev',[
+  grunt.registerTask('dev', [
     'sass',
-    'watch:sass'
+    'uglify:dev',
+    'watch'
   ]);
 
 };
