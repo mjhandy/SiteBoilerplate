0cd167ed2a4f919792849ab33ca8247bf1c97463
Mark Handy <mjhandy@outlook.com>
2020-01-07 12:57:25 -0500

    * even more nav work

--- dist/components/nav/nav.js
+++ dist/components/nav/nav.js
@@ -3,20 +3,28 @@ $(function () {
 
   var $toggler = $('.navbar-toggler'),
     $subToggler = $('.navbar-sub--toggle'),
-    $slide = $('.navbar-slide'),
-    $subNav = $('.navbar-sub');
+    $navBar = $('#navbar');
 
   $toggler.on('click', function () {
-    $slide.toggleClass('show');
+    $navBar.toggleClass('show');
   });
 
-  // $subToggler.hover(
-  //   function () {
-  //     $(this).next().addClass('show');
-  //   },
-  //   function () {
-  //     $(this).next().removeClass('show');
-  //   }
+  $subToggler.on('click', function () {
+    var $this = $(this),
+      $target = $this.attr('data-navtarget'),
+      $ariaExpanded = $this.attr('aria-expanded');
+    console.log($target, $ariaExpanded);
 
-  // );
+    // hide all open sub navs
+    $subToggler.attr('aria-expanded', false);
+    // toggled sub nav
+    if ($ariaExpanded === 'true') {
+      $this.attr('aria-expanded', false);
+    }
+    else {
+      $this.attr('aria-expanded', true);
+    }
+
+
+  })
 });
