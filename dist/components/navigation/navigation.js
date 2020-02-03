f33744a38e79cb522e0b5b1a4ffd812ddf2e5a76
Mark Handy <mjhandy@outlook.com>
2020-02-03 15:49:33 -0500

    * close nav is user clicks else where on the page

--- dist/components/navigation/navigation.js
+++ dist/components/navigation/navigation.js
@@ -16,34 +16,37 @@ $(function () {
     var $this = $(this),
       $ariaExpanded = $this.attr('aria-expanded');
 
-    // hide all open sub navs
-    $subToggle.attr('aria-expanded', false);
+    // hide all open sub navs and reset aria tags
+    ariaReset();
+
     // hide search and reset button state
     $searchButton.attr('aria-expanded', false);
     $searchBox.removeClass('show');
     // toggled sub nav
     if ($ariaExpanded === 'true') {
       $this.attr('aria-expanded', false);
+      $this.next().attr('aria-hidden', false);
     }
     else {
       $this.attr('aria-expanded', true);
+      $this.next().attr('aria-hidden', true);
     }
   });
 
   // show/hide search
   $searchButton.on('click', function () {
-    // hide all open sub navs
-    $subToggle.attr('aria-expanded', false);
     var $this = $(this),
       $ariaExpanded = $this.attr('aria-expanded');
-
-    // 
+    // hide all open sub navs and reset aria tags
+    ariaReset();
+    // toggle aria tag
     if ($ariaExpanded === 'true') {
       $this.attr('aria-expanded', false);
     }
     else {
       $this.attr('aria-expanded', true);
     }
+    // toggle search box
     $searchBox.toggleClass('show');
   });
 
@@ -73,8 +76,28 @@ $(function () {
       ($(window).scrollTop() > $mainNavHeight) ? $makeSmall.addClass('smaller'): $makeSmall.removeClass('smaller');
     });
   }
-
+  // sub nav sticky
   $(window).bind('scroll', function () {
     ($(window).scrollTop() > $mainNavHeight) ? $sectionNav.addClass('sticky'): $sectionNav.removeClass('sticky');
   });
+  // close all open items 
+  // close on ESC key
+  $(document).on('keydown', function (e) {
+    console.log(e);
+    if (e.keyCode === 27) {
+      ariaReset();
+    }
+  });
+  // if the click is not in the nav, close all open nav items
+  $(document).on('click', function (e) {
+    var $target = e.target;
+    if (!$target.closest('.bbNavigation')) {
+      ariaReset();
+    }
+  });
+  // hide all open sub navs and reset aria tags
+  function ariaReset() {
+    $subToggle.attr('aria-expanded', false);
+    $('.nav-sub').attr('aria-hidden', false);
+  }
 });
