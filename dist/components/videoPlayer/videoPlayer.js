1278f128736abd2320b44c0b7bf116e0e3832db8
RIMNET\mahandy <mahandy@blackberry.com>
2020-09-03 10:52:33 -0400

    * video updated * remove security summit

---
+++ dist/components/videoPlayer/videoPlayer.js
@@ -0,0 +1,41 @@
+$(function () {
+  'use strict';
+
+  var $titleCard = $('.video-titleCard'),
+    $endCard = $('.video-endCard'),
+    $video = document.getElementById('video'),
+    $videoTitle = $('.video-title');
+  /*
+    Let's set some initial items
+  */
+  // set the video title
+  $videoTitle.html($video.getAttribute('title'));
+
+  /* 
+    Title Card
+    * On click, fade out the card and start the video playing
+  */
+  $titleCard.on('click', function () {
+    $(this).addClass('hide');
+    $video.play();
+  });
+  /*
+    End card
+    * When the video ends, show the end card
+  */
+  $video.addEventListener('ended', videoEnd, false);
+
+
+
+  /*
+    Custom Functions
+  */
+
+  // when video ends
+  function videoEnd() {
+    $endCard.addClass('show');
+  };
+
+
+
+});
